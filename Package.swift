882261d0a7f1a2f67c2e152af9041f7ae35915ec
Pavel Pavlov <p.pavlov@allterco.com>
2022-03-18 11:47:16 +0200

    Reverted last changes

--- Package.swift
+++ Package.swift
@@ -14,17 +14,17 @@ let package = Package(
             targets: ["MSGHub Notifications iOS", "MSGHubInternal"]),
     ],
     dependencies: [
+        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", branch: "master"),
+        .package(name: "Alamofire",url: "https://github.com/Alamofire/Alamofire.git", branch: "master"),
         .package(url: "https://github.com/devicekit/DeviceKit.git", from: "4.0.0"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "8.13.0")),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.5.0")),
     ],
     targets: [
         .target(
             name: "MSGHub Notifications iOS",
             dependencies: [
                 "Alamofire", "DeviceKit",
-                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAuth", package: "Firebase"),
+                .product(name: "FirebaseMessaging", package: "Firebase"),
             ]
         ),
         .binaryTarget(
