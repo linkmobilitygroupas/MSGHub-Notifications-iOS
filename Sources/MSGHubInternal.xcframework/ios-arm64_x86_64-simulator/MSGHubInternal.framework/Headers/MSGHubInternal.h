8b8491fb1bc4975eb9a658e922e567e315f52803
Pavel Pavlov <p.pavlov@allterco.com>
2022-07-06 18:03:25 +0300

    Updated MSGHubInternal.framework to work on simulator

---
+++ Sources/MSGHubInternal.xcframework/ios-arm64_x86_64-simulator/MSGHubInternal.framework/Headers/MSGHubInternal.h
@@ -0,0 +1,18 @@
+//
+//  MSGHubInternal.h
+//  MSGHubInternal
+//
+//  Created by Pavel Pavlov on 20.01.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MSGHubInternal.
+FOUNDATION_EXPORT double MSGHubInternalVersionNumber;
+
+//! Project version string for MSGHubInternal.
+FOUNDATION_EXPORT const unsigned char MSGHubInternalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSGHubInternal/PublicHeader.h>
+
+
